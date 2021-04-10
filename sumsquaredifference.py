b10c4f5cd4738474ced659fa1e7eaa90904c8fe7
Amy Evans <61657803+evana010@users.noreply.github.com>
2021-04-10 16:51:38 +0100

    Update sumsquaredifference.py

--- sumsquaredifference.py
+++ sumsquaredifference.py
@@ -1,3 +1,4 @@
+#Q. Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 sum_of_squares = 0
 sum = 0
 square_of_sum = 0
@@ -10,3 +11,5 @@ for i in range(1, 101):
 
 answer = square_of_sum - sum_of_squares
 print(answer)
+
+#Answer = 25164150
