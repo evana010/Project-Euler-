afacf81b2b57ff354040d25a6ba1c8b84876653a
Amy Evans <61657803+evana010@users.noreply.github.com>
2021-04-10 16:50:09 +0100

    Update specialtriplet.py

--- specialtriplet.py
+++ specialtriplet.py
@@ -1,6 +1,9 @@
+#Q. A Pythagorean triplet is a set of three natural numbers, a < b < c, for which a2 + b2 = c2. If a + b + c = 1000, find the product abc.
+
 for a in range(0, 1000):
     for b in range(0, 1000):
         for c in range(0,1000):
             if a**2 + b**2 == c**2 and a + b + c == 1000 and a < b < c:
                 print("{}".format(a*b*c))
                 exit()
+#Answer = 31875000
